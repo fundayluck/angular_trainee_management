3a1a5c320feea183f0e41b43e34abad5dd5ad990
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-28 21:27:18 +0700

    dependencies: adding primeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular_trainee_managemant';
+  title = 'trainee';
 }
