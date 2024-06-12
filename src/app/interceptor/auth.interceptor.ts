9cc169c1bcc36c2e31ec1c21bc26a44ed90bbec0
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-06-12 09:02:48 +0700

    feat: condition expiration token

--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -4,15 +4,33 @@ import {
   HttpInterceptorFn,
   HttpRequest,
 } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 import { Observable } from 'rxjs';
+
+interface JwtPayload {
+  exp: number;
+}
 
 export const authInterceptor: HttpInterceptorFn = (
   req: HttpRequest<any>,
   next: HttpHandlerFn
 ): Observable<HttpEvent<any>> => {
   const authToken = localStorage.getItem('token');
+  const router = inject(Router);
+
+  console.log(authToken);
 
   if (authToken) {
+    const decodedToken: JwtPayload = jwtDecode(authToken);
+    const currentTime = Math.floor(Date.now() / 1000);
+
+    if (decodedToken.exp < currentTime) {
+      localStorage.removeItem('token');
+      router.navigate(['/signin']);
+    }
+
     const newReq = req.clone({
       headers: req.headers.append('Authorization', `Bearer ${authToken}`),
     });
